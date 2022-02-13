506449124653d5c9cdd76de9ea725501f91c4de9
itsyefan <ethanyipnz@gmail.com>
2022-02-13 19:13:01 +1300

    Added styling to input fields and button

--- frontend/src/components/InputField.tsx
+++ frontend/src/components/InputField.tsx
@@ -1,11 +1,13 @@
-import React from 'react'
+import React from 'react';
+import "./style.css";
 
 const InputField = () => {
   return (
-    <form>
-      <input type = "input" placeholder = "Enter a source" className="input_box"></input>
-      <input type = "input" placeholder = "Enter a destination" className="input_box"></input>
-      <input type = "input" placeholder = "Enter an arrival time" className="input_box"></input>
+    <form className="input">
+      <input type = "text" placeholder = "Enter a source" className="inputBox"></input>
+      <input type = "text" placeholder = "Enter a destination" className="inputBox"></input>
+      <input type = "time" placeholder = "Enter an arrival time" className="inputBox"></input>
+      <button type="button" className='submitButton'>Submit</button>
     </form>
     
   )
