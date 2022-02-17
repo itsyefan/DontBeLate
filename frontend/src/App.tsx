affc88f12305df9cb035de88494618fd2b73c0f4
itsyefan <ethanyipnz@gmail.com>
2022-02-17 14:50:53 +1300

    Added input functionality, with the field outputting to console

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,14 +1,22 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import InputField from './components/InputField';
 
 const App: React.FC = () => {
+  const [source, setSource] = useState<string>("");
+  const [destination, setDestination] = useState<string>("");
+  const [time, setTime] = useState<string>("");
+
+  console.log(source);
+  console.log(destination);
+  console.log(time);
+
   return (
     <div className="App">
       <link href="https://fonts.googleapis.com/css2?family=Akaya+Telivigala&display=swap" rel="stylesheet"></link>
       
       <span className="heading">Don't Be Late</span>
-      <InputField />
+      <InputField source={source} setSource={setSource} destination={destination} setDestination={setDestination} time={time} setTime={setTime}/>
     </div>
   );
 }
--- frontend/src/components/InputField.tsx
+++ frontend/src/components/InputField.tsx
@@ -1,16 +1,62 @@
 import React from 'react';
 import "./style.css";
 
-const InputField = () => {
+interface Props {
+  source: string;
+  setSource: React.Dispatch<React.SetStateAction<string>>;
+  destination: string;
+  setDestination: React.Dispatch<React.SetStateAction<string>>;
+  time: string;
+  setTime: React.Dispatch<React.SetStateAction<string>>;
+}
+
+const InputField: React.FC<Props> = ({source, setSource, destination, setDestination, time, setTime}) => {
+  const handleSubmit = (e: Event) => {
+    e.preventDefault();
+    // ???
+  }
+
   return (
     //Fields for address and time input
-    <form data-testid="field-container" className="input"> 
-      <input type = "text" placeholder = "Enter a source" data-testid="source-field" className="inputBox"></input>
-      <input type = "text" placeholder = "Enter a destination" data-testid="destination-field" className="inputBox"></input>
-      <input type = "time" placeholder = "Enter an arrival time" data-testid="arrival-field" className="inputBox"></input>
-      <button type="button" data-testid="submit-button" className='submitButton'>Submit</button>
+    <form data-testid="field-container" className="input">
+      <input type="text"
+        placeholder="Enter a source"
+        data-testid="source-field"
+        className="inputBox"
+        value={source}
+        onChange={
+          e => setSource(e.target.value)
+        }
+      />
+
+      <input type="text"
+        placeholder="Enter a destination"
+        data-testid="destination-field"
+        className="inputBox"
+        value={destination}
+        onChange={
+          e => setDestination(e.target.value)
+        }
+      />
+
+      <input type="time"
+        placeholder="Enter an arrival time"
+        data-testid="arrival-field"
+        className="inputBox"
+        value={time}
+        onChange={
+          e => setTime(e.target.value)
+        }
+      />
+
+      <button type="button"
+        data-testid="submit-button"
+        className='submitButton'>
+        Submit
+      </button>
+
     </form>
-    
+
   )
 }
 
