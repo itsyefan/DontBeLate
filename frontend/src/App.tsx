dc366336663133d01cc9da86a1b1e08e40cc8712
itsyefan <ethanyipnz@gmail.com>
2022-02-26 01:51:20 +1300

    Made request to api based on data inputs

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import './App.css';
 import InputField from './components/InputField';
-import { GoogleMap, DistanceMatrixService } from "@react-google-maps/api";
+import { GoogleMap, useJsApiLoader, DistanceMatrixService } from "@react-google-maps/api";
 
 const App: React.FC = () => {
   const [source, setSource] = useState<string>("");
@@ -9,30 +9,60 @@ const App: React.FC = () => {
   const [time, setTime] = useState<string>("");
   const [trafficTime, setTrafficTime] = useState<string>("");
 
-  const service = new google.maps.DistanceMatrixService(); //THIS IS WHERE ITS ALL GOING WRONG :((((
+  const { isLoaded } = useJsApiLoader({
+    id: 'google-map-script',
+    googleMapsApiKey: process.env.REACT_APP_GOOGLE_KEY!
+  })
 
+  if (!isLoaded) {
+    return <div>Loading...</div>
+  }
 
-  console.log(source);
-  console.log(destination);
-  console.log(time);
+  const service = new google.maps.DistanceMatrixService();
+
 
   const getTrafficTime = (e: React.FormEvent) => {
 
-
     e.preventDefault();
 
-    /**const request = {
-      origins: [source],
-      destinations: [destination],
-      travelMode: google.maps.TravelMode.DRIVING,
-      unitSystem: google.maps.UnitSystem.METRIC,
-      avoidHighways: false,
-      avoidTolls: false,
-    };
+    service.getDistanceMatrix(
+      {
+        origins: [source],
+        destinations: [destination],
+        travelMode: google.maps.TravelMode.DRIVING,
+        unitSystem: google.maps.UnitSystem.METRIC,
+        avoidHighways: false,
+        avoidTolls: false,
+      }, callback
+      );
 
-    (document.getElementById("request") as HTMLDivElement).innerText =
-    JSON.stringify(request, null, 2);**/
+      function callback(response: any, status: any) {
+        if (status == 'OK') {
 
+          var origins = response.originAddresses;
+          var destinations = response.destinationAddresses;
+
+          for (var i = 0; i < origins.length; i++) {
+            var results = response.rows[i].elements;
+            
+            for (var j = 0; j < results.length; j++) {
+              var element = results[j];
+              var distance = element.distance.text;
+              var duration = element.duration.text;
+              console.log(duration);
+              var from = origins[i];
+              var to = destinations[j];
+            }
+          }
+        }
+        else {
+          console.log("Address was not found.")
+        }
+      }
+
+    //setSource("");
+    //setDestination("");
+    //setTime("");
   };
 
 
