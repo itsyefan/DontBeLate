3519ec4d11d85726ec68aaf38730dfbbde4d763e
itsyefan <ethanyipnz@gmail.com>
2022-03-12 22:20:33 +1300

    Refactor callback to remove for loop

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -34,31 +34,37 @@ const App: React.FC = () => {
         avoidHighways: false,
         avoidTolls: false,
       }, callback
-      );
+    );
 
-      function callback(response: google.maps.DistanceMatrixResponse | null, status: google.maps.DistanceMatrixStatus) {
-        if (response != null) {
+    function callback(response: google.maps.DistanceMatrixResponse | null, status: google.maps.DistanceMatrixStatus) {
+      if (response != null) {
+        var origins = response.originAddresses;
+        var destinations = response.destinationAddresses;
 
-          var origins = response.originAddresses;
-          var destinations = response.destinationAddresses;
+        var results = response.rows[0].elements;
 
-          for (var i = 0; i < origins.length; i++) {
-            var results = response.rows[i].elements;
-            
-            for (var j = 0; j < results.length; j++) {
-              var element = results[j];
-              var distance = element.distance.text;
-              var duration = element.duration.text;
-              console.log(duration);
-              var from = origins[i];
-              var to = destinations[j];
-            }
+        var element = results[0];
+        var duration = element.duration.text;
+        var from = origins[0];
+        var to = destinations[0];
+
+        console.log(duration);
+
+        /** 
+        for (var i = 0; i < origins.length; i++) {
+          var results = response.rows[i].elements;
+
+          for (var j = 0; j < results.length; j++) {
+
           }
         }
-        else {
-          console.log("Address was not found.")
-        }
+
+        */
       }
+      else {
+        console.log("Address was not found.")
+      }
+    }
   };
 
 
