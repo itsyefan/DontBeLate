2290531d671b444b6432967c4bf55bda5ac9d860
itsyefan <ethanyipnz@gmail.com>
2022-04-19 19:20:41 +1200

    displayed all the habits on screen

--- frontend/src/pages/HabitsPage.tsx
+++ frontend/src/pages/HabitsPage.tsx
@@ -1,19 +1,39 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { getAuth, signOut } from 'firebase/auth';
 import { uid } from 'uid';
-import { set, ref, getDatabase } from 'firebase/database';
+import { set, ref, getDatabase, onValue } from 'firebase/database';
 
 function HabitsPage() {
     const [habit, setHabit] = useState("");
+    const [habits, setHabits] = useState<any[]>([]);
 
     const auth = getAuth();
     const db = getDatabase();
+
+    useEffect(() => {
+        auth.onAuthStateChanged((user) => {
+            if (user) {
+                if(auth.currentUser){
+                    onValue(ref(db, `/${auth.currentUser.uid}`), snapshot => {
+                        setHabits([]);
+                        const data = snapshot.val();
+                        if(data !== null){
+                            Object.values(data).map((habit) => {
+                                setHabits((oldArray) => [...oldArray, habit]);
+                            });
+                        }
+                    });
+                }
+            }
+        });
+        
+    }, [auth])
 
     //Adding habits
     const writeToDatabase = () => {
         const habitId = uid();
 
-        if (auth.currentUser != null) {
+        if (auth.currentUser) {
             set(ref(db, `/${auth.currentUser.uid}/${habitId}`), {
                 habit: habit,
                 habitId: habitId,
@@ -32,6 +52,11 @@ function HabitsPage() {
                 value={habit}
                 onChange={(e: React.ChangeEvent<HTMLInputElement>) => setHabit(e.target.value)}
             />
+            {
+                habits.map((habit) => (
+                    <h3>{habit.habit}</h3>
+                ))
+            }
             <button onClick={writeToDatabase}>Add</button>
             <button onClick={() => signOut(auth)}>Sign Out</button>
         </div>
